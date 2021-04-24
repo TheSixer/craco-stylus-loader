4384dfdf18a7313ffa95bea05fc108d28027b70a
张龙佳 <zhanglongjia@corp.netease.com>
2021-04-24 18:10:13 +0800

    Support to work with craco-antd

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
+const path = require("path");
 import { Configuration, RuleSetQuery } from "webpack";
 
-const STYL_REGEX = /\.styl$/;
-const STYL_MODULE_REGEX = /\.module\.styl$/;
-
-export const overrideWebpackConfig = ({
+const overrideWebpackConfig = ({
   webpackConfig,
-  context: { env }
+  context: { env },
+  pluginOptions,
 }: {
   webpackConfig: Configuration;
   context: { env: string };
+  pluginOptions: any;
 }) => {
+  const {
+    getLoader,
+    loaderByName,
+    throwUnexpectedConfigError
+  } = require("@craco/craco");
+
+  // This is mocked in Windows tests
+  const pathSep = module.exports.pathSep;
+
+  const throwError = (message: any, githubIssueQuery: any) =>
+    throwUnexpectedConfigError({
+      packageName: "craco-stylus-loader",
+      githubRepo: "TheSixer/craco-stylus-loader",
+      message,
+      githubIssueQuery
+    });
+
+  const stylusExtension = /\.styl$/;
+  const stylusModuleExtension = /\.module\.styl$/;
+
+  pluginOptions = pluginOptions || {};
+
   const mode = env === "development" ? "dev" : "prod";
   const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
   // Need these for production mode, which are copied from react-scripts
@@ -66,15 +88,15 @@ export const overrideWebpackConfig = ({
     loaders.length - 1,
     0,
     {
-      test: STYL_REGEX,
-      exclude: STYL_MODULE_REGEX,
+      test: stylusExtension,
+      exclude: stylusModuleExtension,
       use: getStylusLoader({
         importLoaders: 2
       }),
       sideEffects: mode === "prod"
     },
     {
-      test: STYL_MODULE_REGEX,
+      test: stylusModuleExtension,
       use: getStylusLoader({
         importLoaders: 2,
         modules: {
@@ -84,5 +106,24 @@ export const overrideWebpackConfig = ({
     }
   );
 
+  const { isFound, match: fileLoaderMatch } = getLoader(
+    webpackConfig,
+    loaderByName("file-loader")
+  );
+
+  if (!isFound) {
+    throwError(
+      `Can't find file-loader in the ${env} webpack config!`,
+      "webpack+file-loader"
+    );
+  }
+  fileLoaderMatch.loader.exclude.push(stylusExtension);
+
   return webpackConfig;
+};
+
+// pathSep is mocked in Windows tests
+module.exports = {
+  overrideWebpackConfig,
+  pathSep: path.sep
 };
